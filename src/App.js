32e72c16bc62783beba6ca0afa97ff64ad9f85ae
JuliaPutrova <Julia.putrova@gmail.com>
2021-10-09 11:12:06 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import Filter from './components/Filter/Filter';
 
+const initialsState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    // contacts: [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
@@ -30,7 +38,8 @@ class App extends Component {
       number,
     };
 
-    //проверка на дублируемый номер телефона
+    //проверка на дублируемый номер телефона (заменить на  some!!)
+
     this.state.contacts.find(
       contact =>
         contact.name.toLowerCase() === addNewContact.name.toLowerCase(),
@@ -40,6 +49,28 @@ class App extends Component {
           contacts: [addNewContact, ...prevState.contacts],
         }));
   };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  };
+
+  // componentDidMount = () => {
+  //   const contacts = JSON.parse(localStorage.getItem('contacts'));
+  //   if (contacts !== null) {
+  //     this.setState({
+  //       contacts,
+  //     });
+  //   }
+  // };
+
+  componentDidMount = () => {
+    const newContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({
+      contacts: newContacts ?? initialsState,
+    });
+  };
+
+  // оператор объединения с null https://learn.javascript.ru/nullish-coalescing-operator
 
   //метод для удаления контакта
   onDeleteContact = id => {
